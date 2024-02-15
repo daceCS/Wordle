848c17d49b2374a78761f5f302228e38181366b0
David Acevedo <58893468+daceCS@users.noreply.github.com>
2024-02-15 09:12:55 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,41 +1,13 @@
-from termcolor import colored
-import random
-import sys
+import numpy as np
+import pickle
+import argparse
+import os
 
+from state import State
+from player import HumanPlayer
 
-def read_random_word():
-    with open("words.txt") as f:
-        word_array = f.read().splitlines()
-        return random.choice(word_array)
+if __name__ == "__main__":
+    p1 = HumanPlayer("human1")
 
-
-print("Let's play Wordle:")
-print("Type a 5 letter word below and press Enter. You have 6 tries to guess the random word.\n")
-print("x means the letter is not in the word")
-print("o means the letter is in the word and in the wrong spot")
-print("! means the letter is in the word and in the correct spot")
-
-word = read_random_word()
-print(word)
-
-for attempt in range(1, 7):
-    guess = input().lower()
-
-
-    sys.stdout.write('\x1b[1A')
-    sys.stdout.write('\x1b[2K')
-    output_string = ""
-
-    for i in range(min(len(guess), 5)):
-        if guess[i] == word[i]:
-            output_string += "!"
-        elif guess[i] in word:
-            output_string += "o"
-        else:
-            output_string += "x"
-    print(output_string)
-
-    if guess == word:
-        print("Congratulations! You guessed the word in %i guesses." % attempt)
-    elif attempt == 6:
-        print("You didn't guess the word within 6 tries, it was '%s'" % word)
+    st = State(p1)
+    st.playGame()
